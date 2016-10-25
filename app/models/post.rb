a01fcc050ff9eba9751ae11a09c4f60affb26e60
James Segarra III <JamesSegarra@gmail.com>
2016-10-25 08:23:18 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
+class Clickbait < ActiveModel::Validator
+  CLICKBAIT=["Won't Believe", "Secret", "Top
+  #{/([0-9])/}", "Guess"]
+
+  def validate(record)
+    answer = false
+    CLICKBAIT.each do |bait|
+      answer = record.title.to_s.include?(bait)
+      break if answer
+    end
+    record.errors[:clickbait] << "Not Clickbait!" unless answer
+    # binding.pry
+    answer
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with Clickbait
+  validates :title, presence: true
+  validates :content, length: {minimum:250}
+  validates :summary, length: {maximum:250}
+  validates :category, inclusion:{in:%w(Fiction Non-Fiction)}
+
 end
